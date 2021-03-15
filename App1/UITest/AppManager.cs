f9ffa0d8e7fb9313a9fea056bf85f816ac59096a
Gummimundur <gudm.mar@hotmail.com>
2021-03-15 13:32:54 +0000

    Update AppManager.cs

--- App1/UITest/AppManager.cs
+++ App1/UITest/AppManager.cs
@@ -55,7 +55,6 @@ namespace UITest
                 app = ConfigureApp
                     .iOS
                     .EnableLocalScreenshots()
-                    .PreferIdeSettings()
                     .DeviceIdentifier(simId)
                     .AppBundle("../../../../binaries/App1.iOS/bin/iPhoneSimulator/Release/App1.iOS.app")
                     .StartApp();
